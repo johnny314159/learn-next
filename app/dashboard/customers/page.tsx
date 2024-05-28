f2a61993a5aaa3c81ba022e2bd9b01d3969fda23
Johnny Liu <mayaisbad@gmail.com>
2024-05-28 17:02:26 -0400

    Added metadata, search, form validation, auth and re-rendering

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,29 @@
-export default function Page (){
-    return <p>Customers Page</p>
+import { fetchFilteredCustomers } from "@/app/lib/data"
+import { Customer, FormattedCustomersTable } from "@/app/lib/definitions"
+import Table from "@/app/ui/customers/table"
+import { lusitana } from "@/app/ui/fonts"
+import Pagination from "@/app/ui/invoices/pagination"
+import Search from "@/app/ui/search"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Customers',
 }
+
+export default async function CustomersPage (
+    {searchParams}:{
+        searchParams?: {
+            query?: string;
+        }; 
+    })
+    {
+    const query = searchParams?.query || '';
+ 
+    // not in tutorial, implement Customers table with search.
+    return (
+        <div className="w-full">
+            <Table query={query}></Table>
+        </div>
+    )
+}
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import '@/app/ui/global.css';
 import { inter } from  '@/app/ui/fonts';
+import { StrictMode } from 'react';
+import SideNav from './ui/dashboard/sidenav';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: ' The official Next.js Course Dashboard, built with App Router',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
+};
 
 export default function RootLayout({
   children,
@@ -7,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
+    <StrictMode>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
+    </StrictMode>
   );
 }
